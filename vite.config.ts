a73e1b17719bf82138ee53d764648852774370d5
Muhammad Rahman <mrahman9801@gmail.com>
2026-06-07 21:50:07 +0600

    feat: deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  base: '/gallerium/',
   plugins: [basicSsl()],
   server: {
     host: true,
